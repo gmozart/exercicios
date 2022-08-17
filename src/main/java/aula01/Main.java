ae766fe0c9c6fc749d5beadedc9a0626416964d3
gmozart <mozart.gleison@gmail.com>
2022-08-17 16:33:54 -0300

    Exercicio finalizado

--- src/main/java/aula01/Main.java
+++ src/main/java/aula01/Main.java
@@ -13,13 +13,21 @@ public class Main {
         Calculadora.subtracao(23,10099);
         Calculadora.multiplicacao(23,450);
 
+        /*Mensagem*/
+
         System.out.println("Exercicio Mensagem");
 
         Mensagem.obeterMensagem(5);
         Mensagem.obeterMensagem(4);
-        Mensagem.obeterMensagem(22);
+        Mensagem.obeterMensagem(17);
 
+        /*Emprestimo*/
 
+        System.out.println("Exercicio Emprestimo");
+
+        Emprestimo.calcular(1000,Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000,Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000,5);
 
 
 
